17d9b57821e4e653e979882fc5b459644af66520
ideepakrajput <deepakkumar18112001@gmail.com>
2023-11-22 12:51:26 +0530

    aws server added

--- constants/baseApiUrl.js
+++ constants/baseApiUrl.js
@@ -1,2 +1,3 @@
-export const BASE_API_URL = 'https://tring-tring-server.onrender.com/';
+// export const BASE_API_URL = 'https://tring-tring-server.onrender.com/';
+export const BASE_API_URL = 'http://ec2-3-110-37-161.ap-south-1.compute.amazonaws.com:8080/';
 // export const BASE_API_URL = 'http://localhost:8080/';
