01786e895e513c9de2c4c1604d6d167a45b415be
ideepakrajput <deepakkumar18112001@gmail.com>
2023-11-25 18:34:36 +0530

    account frame done

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,5 +1,5 @@
 import { useState, useContext } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, SafeAreaView, StyleSheet } from 'react-native';
 import SelectPicker from '../components/SelectPicker';
 import NotificationSwitch from '../components/NotificationSwitch';
 import { AuthContext } from '../context/authContext';
@@ -8,7 +8,10 @@ import axios from 'axios';
 import { BASE_API_URL } from "../constants/baseApiUrl";
 import FooterMenu from '../components/Menus/FooterMenu';
 import ProfileDetailsCard from '../components/ProfileDetails';
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import { MaterialIcons, Ionicons } from '@expo/vector-icons';
+import COLORS from '../constants/colors';
+const config = require("../package.json");
+const appVersion = config.version;
 
 export default function Settings() {
   const { state, setState } = useContext(AuthContext);
@@ -46,12 +49,12 @@ export default function Settings() {
   }
 
   return (
-    <>
-      <View
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* <View
         style={{
           flex: 3,
           alignItems: "center",
-          justifyContent: "space-evenly"
+          justifyContent: "space-between"
         }}>
         <ProfileDetailsCard />
         <View>
@@ -78,10 +81,69 @@ export default function Settings() {
             />
           </TouchableOpacity>
         </View>
+      </View> */}
+      <View style={{ flex: 1, paddingHorizontal: 16, paddingVertical: 24, justifyContent: "space-between" }}>
+        <View>
+          <View style={styles.box}>
+            <View>
+              <MaterialIcons style={styles.iconStyle} name="mode-edit" size={24} color="black" />
+            </View>
+            <View style={styles.card}>
+              <Text style={styles.text1}>Basic Details</Text>
+              <Text style={styles.text2}>Name, Phone Number</Text>
+            </View>
+          </View>
+
+          <View style={styles.box}>
+            <View>
+              <MaterialIcons style={styles.iconStyle} name="language" size={24} color="black" />
+            </View>
+            <View style={styles.card}>
+              <Text style={styles.text1}>Language Settings</Text>
+              <Text style={styles.text2}>Choose your Preferred Language</Text>
+            </View>
+          </View>
+
+          <View style={styles.box}>
+            <Ionicons style={styles.iconStyle} name="settings-outline" size={24} color="black" />
+            <Text style={[styles.card, styles.text1]}>App Settings</Text>
+          </View>
+        </View>
+        <Text style={[styles.text2, { paddingBottom: 24 }]}>App Version v{appVersion}</Text>
       </View>
-      <View style={{ justifyContent: "flex-end" }}>
+      <View style={{ flex: 0.1, justifyContent: "flex-end" }}>
         <FooterMenu />
       </View>
-    </>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    flexDirection: "row",
+    gap: 16,
+    paddingBottom: 8,
+    borderBottomWidth: 1,
+    marginBottom: 8,
+  },
+  iconStyle: {
+    backgroundColor: COLORS.history,
+    borderRadius: 50,
+    padding: 14
+  },
+  card: {
+    alignSelf: "center",
+    gap: 4
+  },
+  text1: {
+    fontSize: 16,
+    fontFamily: "lato-reg",
+    fontWeight: 600,
+  },
+  text2: {
+    fontSize: 12,
+    fontFamily: "lato-reg",
+    fontWeight: 400,
+    color: COLORS.secondary
+  },
+})
